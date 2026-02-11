242f75bab8296091a6e30f59ac3d03e76322cece
Aryan Sinha <151495894+A-ryan06@users.noreply.github.com>
2026-02-11 22:57:22 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (66.65%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numTrees(int n) {
+
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int nodes = 2; nodes <= n; nodes++) {
+
+            int total = 0;
+
+            for (int root = 1; root <= nodes; root++) {
+                total += dp[root - 1] * dp[nodes - root];
+            }
+
+            dp[nodes] = total;
+        }
+
+        return dp[n];
+    }
+}
