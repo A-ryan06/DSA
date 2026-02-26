f61197342c9993be8d3ba08927fae2d4fd311a2f
Aryan Sinha <151495894+A-ryan06@users.noreply.github.com>
2026-02-26 22:25:40 +0530

    Time: 32 ms (49.35%) | Memory: 46.5 MB (50.72%) - LeetSync

---
+++ 149-max-points-on-a-line/max-points-on-a-line.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public int maxPoints(int[][] points) {
+        if (points.length <= 2) return points.length;
+
+        int max = 0;
+
+        for (int i = 0; i < points.length; i++) {
+            Map<String, Integer> map = new HashMap<>();
+            int duplicates = 1;
+            int currMax = 0;
+
+            for (int j = i + 1; j < points.length; j++) {
+                int dx = points[j][0] - points[i][0];
+                int dy = points[j][1] - points[i][1];
+
+                // Duplicate point
+                if (dx == 0 && dy == 0) {
+                    duplicates++;
+                    continue;
+                }
+
+                int gcd = gcd(dx, dy);
+                dx /= gcd;
+                dy /= gcd;
+
+                String slope = dy + "/" + dx;
+                map.put(slope, map.getOrDefault(slope, 0) + 1);
+
+                currMax = Math.max(currMax, map.get(slope));
+            }
+
+            max = Math.max(max, currMax + duplicates);
+        }
+
+        return max;
+    }
+
+    private int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+}
